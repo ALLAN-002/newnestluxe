8c3e171b220db9c57ced2b92bc23fe6d10f63493
Allan Odhiambo <odhiamboallan002@gmail.com>
2025-06-07 22:45:43 +0300

    fix

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -8,10 +8,10 @@ import finder from "../assets/finder.png";
 
 export default function Partners() {
   return (
-    <div className="bg-gray-100 max-w-full ml-50">
+    <div className="bg-gray-100 max-w-full ml-20">
       <div className="bg-white rounded-lg p-10 max-w-7xl mx-auto mt-10">
         <h1 className="text-3xl font-bold mb-8 text-center">Our Partners</h1>
-        <div className="flex flex-wrap justify-center gap-10">
+        <div className="flex flex-wrap justify-center gap-5">
           <img src={finder} alt="Finder logo" className="w-32 sm:w-40 h-20 sm:h-28 object-contain" />
           <img src={elegance} alt="Elegance logo" className="w-32 sm:w-40 h-20 sm:h-28 object-contain rounded-lg" />
           <img src={backpacker} alt="Backpacker logo" className="w-32 sm:w-40 h-20 sm:h-28 object-contain rounded-lg" />
