29af29bd7950392bcd0fc7c5f3078d5bab18df22
Allan Odhiambo <odhiamboallan002@gmail.com>
2025-06-07 22:54:02 +0300

    fix

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -6,9 +6,9 @@ import landscape from "../assets/landscape.jpeg";
 const Services = () => {
   return (
     <div className="bg-white ml-40 p-6 md:p-10 mx-auto max-w-screen-xl justify-center ">
-      <h1 className="text-3xl md:text-4xl text-[rgb(27,38,59)] font-semibold ml-120">SERVICES</h1>
+      <h1 className="text-3xl md:text-4xl text-[rgb(27,38,59)] font-semibold ml-80">SERVICES</h1>
       
-      <p className="text-3xl md:text-5xl font-bold text-[rgb(27,38,59)] leading-tight justify-items-center ml-108 mt-4">
+      <p className="text-3xl md:text-5xl font-bold text-[rgb(27,38,59)] leading-tight justify-items-center ml-75 mt-4">
         Your Path to <br /> Easy Lifestyle
       </p>
       
